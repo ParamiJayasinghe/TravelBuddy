607a268eff0a304cb263ba1fcaa1825f11922c63
ParamiJayasinghe <apurvajayasinghe@gmail.com>
2025-01-11 23:55:00 +0530

    welcome page modified

--- travel-tour-app/src/pages/Welcome/welcome.js
+++ travel-tour-app/src/pages/Welcome/welcome.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import './welcome.css';
 
-const Welcome = ({ loginUser }) => {
+const Welcome = () => {
   const navigate = useNavigate();
 
   const handleLoginClick = () => {
@@ -15,9 +15,11 @@ const Welcome = ({ loginUser }) => {
 
   return (
     <div className="welcome-container">
-      <h1>Welcome to Our Travel and Tour App!</h1>
-      <button className="welcome-btn" onClick={handleLoginClick}>Login</button>
-      <button className="welcome-btn" onClick={handleRegisterClick}>Register</button>
+      <h1 className="welcome-title">Welcome to Our Travel and Tour App!</h1>
+      <div className="welcome-btn-container">
+        <button className="welcome-btn" onClick={handleLoginClick}>Login</button>
+        <button className="welcome-btn" onClick={handleRegisterClick}>Register</button>
+      </div>
     </div>
   );
 };
